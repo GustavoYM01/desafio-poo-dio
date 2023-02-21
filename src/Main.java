3ceff71d64e1a96b6268785e772425ea070b15cb
GustavoYM01 <gumatimoto@hotmail.com>
2023-02-21 14:17:57 -0300

    Heranca e polimorfismo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.time.LocalDate;
 
+import br.com.dio.desafio.dominio.Conteudo;
 import br.com.dio.desafio.dominio.Curso;
 import br.com.dio.desafio.dominio.Mentoria;
 
@@ -12,8 +13,15 @@ public class Main {
 		Mentoria mentoria = new Mentoria("Avancando em Java", 
 				"Aprenda mais sobre essa linguagem orientada a objetos", LocalDate.now());
 		
+		Conteudo conteudo1 = new Curso("Estrutura de dados", "Principais conceitos", 10);
+		Conteudo conteudo2 = new Mentoria("Avancando em estrutura de dados", 
+				"Aprenda mais sobre estrutura de dados", LocalDate.now());
+		
 		System.out.println(curso);
 		System.out.println(mentoria);
+		
+		System.out.println(conteudo1);
+		System.out.println(conteudo2);
 
 	}
 
