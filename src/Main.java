208dfdc34bb5e617ca47c70608d3755adcb95aa5
GustavoYM01 <gumatimoto@hotmail.com>
2023-02-21 19:06:56 -0300

    finalizacao

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
 import java.time.LocalDate;
 
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Conteudo;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Curso curso = new Curso("Java POO", "Do basico ao intermediario", 8);
+		Curso curso1 = new Curso("Java POO", "Do basico ao intermediario", 8);
+		
+		Curso curso2 = new Curso("JavaScript", "Do basico ao avancado", 14);
 		
 		Mentoria mentoria = new Mentoria("Avancando em Java", 
 				"Aprenda mais sobre essa linguagem orientada a objetos", LocalDate.now());
@@ -18,11 +22,29 @@ public class Main {
 		Conteudo conteudo2 = new Mentoria("Avancando em estrutura de dados", 
 				"Aprenda mais sobre estrutura de dados", LocalDate.now());
 		
-		System.out.println(curso);
-		System.out.println(mentoria);
+		Bootcamp bootcamp = new Bootcamp();
 		
-		System.out.println(conteudo1);
-		System.out.println(conteudo2);
+		bootcamp.setNome("Bootcamp Java Master");
+		bootcamp.setDescricao("Bootcamp para impulsionar seus conhecimentos em Java");
+		bootcamp.getConteudos().add(curso1);
+		bootcamp.getConteudos().add(curso2);
+		bootcamp.getConteudos().add(mentoria);
+		
+		Dev devPedro = new Dev();
+		
+		devPedro.setNome("Pedro");
+		devPedro.inscreverBootcamp(bootcamp);
+		
+		System.out.println("Conteudos em que Pedro esta inscrito: " + devPedro.getConteudosInscritos());
+		
+		devPedro.progredir();
+		devPedro.progredir();
+		devPedro.progredir();
+		
+		System.out.println("==================");
+		System.out.println("Conteudos em que Pedro esta inscrito: " + devPedro.getConteudosInscritos());
+		System.out.println("Conteudos que Pedro concluiu: " + devPedro.getConteudosConcluidos());
+		System.out.println("XP: " + devPedro.calcularTotalXp());
 
 	}
 
